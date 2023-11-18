25dc8c6a5c19171d2adc15205c6d7bbc665740ff
stventek <xenizs@protonmail.com>
2023-11-18 17:24:33 -0400

    make task description nulleable

--- task/models.py
+++ task/models.py
@@ -6,7 +6,7 @@ class Task(BaseModel):
 	user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
 	priority = models.DecimalField(max_digits=20, decimal_places=10)
 	section = models.ForeignKey(Section, on_delete=models.CASCADE, related_name='tasks')
-	description = models.TextField()
+	description = models.TextField(default=None, blank=True, null=True)
 	name = models.CharField(max_length=255)
 
 	def __str__(self):
